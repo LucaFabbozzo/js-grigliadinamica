5607ce35d64ad6279305292cdf9ba5e617bebc0b
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-30 12:43:43 -0500

    [add] html structure with squares

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Generare una griglia 8x8 di 64 quadratini.
+// Ogni quadratino ha un numero random unico da 1 a 64.
+// Cliccando sul quadratino vedo il numero
+// quelli pari sono verti quelli dispari sono rossi, 
+//il numero non si ripete mai anche se sono tutti mischiati, 
+//la posizione dei quadratini è sempre random ad ogni reset.
+
+// 1 creare la struttura HTML / CSS
+// 2 creare la griglia dinamicamente
+// 3 numerare i quadratini
+// 4 gestire il click del quadratino
